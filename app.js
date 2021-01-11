310ca069e7e7e9aa0dd6fd78e2df4ff59faa4dcf
Audra <73372336+IamAudra@users.noreply.github.com>
2021-01-10 20:03:35 -0500

    Added Success/Error msg prep for CC5b

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const regForm = document.getElementById('registration-form')
 const username = document.getElementById('username')
 const password = document.getElementById('password')
 const email = document.getElementById('email')
+const Confirmpassword = document.getElementById('password-2')
 const usernameErr = username.nextElementSibling
 const passwordErr = password.nextElementSibling
 const emailErr = email.nextElementSibling
+const ConfirmpasswordErr = Confirmpassword.nextElementSibling
+const usernameSuccess = usernameErr.nextElementSibling
+const emailSuccess = emailErr.nextElementSibling
+const passwordSucess = passwordErr.nextElementSibling
+const ConfirmpasswordSuccess = ConfirmpasswordErr.nextElementSibling
+
 // const usernameErr = document.getElementById("usernameErr")
 
 // 2. Add event / modification
@@ -18,7 +25,7 @@ regForm.addEventListener('click', function (e) {
     // 3. If empty, give user some feedback
     usernameErr.className = 'block bg-red-500 text-white'
   } else {
-   usernameSuccess.className = 'block bg-green- 600 text-white'
+   usernameSuccess.className = 'block bg-green-600 text-white'
   }
 
  //@TODO: add the validation for email
@@ -36,12 +43,14 @@ if (password.value === '') {
   passwordErr.className = 'block bg-red-600 text-white'
 }
 else {
-  passwordSucess .className = 'block bg-green-600 text-white'
+  passwordSucess.className = 'block bg-green-600 text-white'
  }
 
-ConfirmpassworErr.className = 'bg-red-500 text-white'
-}
-else {
-ConfirmpassworSuccess.className = 'block bg-green-600 text-white'
-}
-)
+
+ if (Confirmpassword.value === 'password.value'){
+   ConfirmpasswordErr.className = 'block bg-red-600 text-white'
+ }
+ else {
+   ConfirmpasswordSuccess.className = 'block bg-green-600 text-white'
+ }
+})
